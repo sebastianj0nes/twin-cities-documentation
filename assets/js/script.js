f79b2783152845a2b3c160f35295c826bef853ea
Seb <jonesseb10@gmail.com>
2023-02-17 22:04:53 +0000

    initialise config section, fix linking between pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,67 @@
 
 // Initialise HTML elements 
 // BUTTONS
-var modelButton = $(".modelBut");
-var homeButton = $(".homeBut");
-var configButton = $(".configBut");
+const modelButton = $(".modelBut");
+const homeButton = $(".homeBut");
+const configButton = $(".configBut");
 
 // DIVS
-var modelDiv = $("#modelDiv");
-var bioDiv = $("#bioDiv");
+const modelDiv = $("#modelDiv");
+const bioDiv = $("#bioDiv");
+const configDiv = $("#configDiv");
 
 
 
 
 // HOME 
 homeButton.on("click",() => {
-    modelDiv.addClass("hide");
     bioDiv.removeClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.addClass("hide");
 })
 
 // MODELS & DESIGN
 // Event listener for click on model button
+
+// Homepage button
 modelButton.on("click", () => {
     bioDiv.addClass("hide");
     modelDiv.removeClass("hide");
     $(".modelBut1").addClass("loaded");
+    $(".configBut1").removeClass("loaded");
+})
+// Side navbar button
+$(".modelBut1").on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.removeClass("hide");
+    configDiv.addClass("hide");
+    $(".modelBut1").addClass("loaded");
+    $(".configBut1").removeClass("loaded");
 })
 
+
+
+
+
+
+
+
+// CONFIGURATION FILE
+// Event listener for click on config button
 configButton.on("click", () => {
     bioDiv.addClass("hide");
     modelDiv.addClass("hide");
+    configDiv.removeClass("hide");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").addClass("loaded");
+})
+
+$(".configBut1").on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.removeClass("hide");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").addClass("loaded");
 } )
 
 
