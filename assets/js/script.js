e9154d8e58ddde45802e974cf6d1a930cc6807b7
Seb <jonesseb10@gmail.com>
2023-02-18 01:53:54 +0000

    update page structure link with js, initialise api pages to add content to

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,72 +2,135 @@
 
 // Initialise HTML elements 
 // BUTTONS
-const modelButton = $(".modelBut");
 const homeButton = $(".homeBut");
+const modelButton = $(".modelBut");
 const configButton = $(".configBut");
+const owButton = $(".owBut");
+const gmButton = $(".gmBut");
 
 // DIVS
-const modelDiv = $("#modelDiv");
 const bioDiv = $("#bioDiv");
+const modelDiv = $("#modelDiv");
 const configDiv = $("#configDiv");
+const owDiv = $("#owDiv");
+const gmDiv = $("#gmDiv");
 
 
-
-
-// HOME 
+// HOME BUTTON (navbar)
 homeButton.on("click",() => {
     bioDiv.removeClass("hide");
     modelDiv.addClass("hide");
     configDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.addClass("hide");
 })
 
-// MODELS & DESIGN
-// Event listener for click on model button
+
+// MODELS & DESIGN 
 
 // Homepage button
 modelButton.on("click", () => {
     bioDiv.addClass("hide");
+    configDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.addClass("hide");
     modelDiv.removeClass("hide");
     $(".modelBut1").addClass("loaded");
     $(".configBut1").removeClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("loaded");
 })
 // Side navbar button
 $(".modelBut1").on("click", () => {
     bioDiv.addClass("hide");
-    modelDiv.removeClass("hide");
     configDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.addClass("hide");
+    modelDiv.removeClass("hide");
     $(".modelBut1").addClass("loaded");
     $(".configBut1").removeClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("loaded");
 })
 
-
-
-
-
-
-
-
 // CONFIGURATION FILE
-// Event listener for click on config button
+
+// Homepage button
 configButton.on("click", () => {
     bioDiv.addClass("hide");
     modelDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.addClass("hide");
     configDiv.removeClass("hide");
-    $(".modelBut1").removeClass("loaded");
     $(".configBut1").addClass("loaded");
+    $(".modelBut1").removeClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("loaded");
 })
-
+// Side navbar button
 $(".configBut1").on("click", () => {
     bioDiv.addClass("hide");
     modelDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.addClass("hide");
     configDiv.removeClass("hide");
-    $(".modelBut1").removeClass("loaded");
     $(".configBut1").addClass("loaded");
-} )
+    $(".modelBut1").removeClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("loaded");
+})
 
 
+// OPENWEATHER API
 
+// Homepage button
+owButton.on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.addClass("hide");
+    gmDiv.addClass("hide");
+    owDiv.removeClass("hide");
+    $(".owBut1").addClass("loaded");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("hide");
+})
+// Side navbar button
+$(".owBut1").on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.addClass("hide");
+    gmDiv.addClass("hide");
+    owDiv.removeClass("hide");
+    $(".owBut1").addClass("loaded");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").removeClass("loaded");
+    $(".gmBut1").removeClass("loaded");
+})
 
+// GOOGLE MAPS API
 
-
-// Remove content function
+// Homepage button
+gmButton.on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.removeClass("hide");
+    $(".gmBut1").addClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").removeClass("loaded");
+})
+// Side navbar button
+$(".gmBut1").on("click", () => {
+    bioDiv.addClass("hide");
+    modelDiv.addClass("hide");
+    configDiv.addClass("hide");
+    owDiv.addClass("hide");
+    gmDiv.removeClass("hide");
+    $(".gmBut1").addClass("loaded");
+    $(".owBut1").removeClass("loaded");
+    $(".modelBut1").removeClass("loaded");
+    $(".configBut1").removeClass("loaded");
+})
